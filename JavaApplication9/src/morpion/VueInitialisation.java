9697ff17023612838c59cc057bb734f58beb55cf
rousstan <rousstan@pc-dg-037-05.iut2.upmf-grenoble.fr>
2018-12-07 10:44:51 +0100

    coucou

---
+++ JavaApplication9/src/morpion/VueInitialisation.java
@@ -0,0 +1,36 @@
+package morpion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rousstan
+ */
+        
+        
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class VueInitialisation {
+    
+    
+    window = new JFrame();
+    window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+}
