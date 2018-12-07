e82fd59d8c34392e935109bf179c2b0b5efbce88
rousstan <rousstan@pc-dg-037-05.iut2.upmf-grenoble.fr>
2018-12-07 11:37:19 +0100

    coucou

--- JavaApplication9/src/morpion/Morpion.java
+++ JavaApplication9/src/morpion/Morpion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication9;
+package morpion;
 
 /**
  *
@@ -16,6 +16,10 @@ public class Morpion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        VueInitialisation test;
+        test = new VueInitialisation();
+        test.afficher();
     }
     
 }
